b84a0fa5f4be014c4cb75a26ecbcd260c5c2426c
Paolo Cremonese <cremonesep25@gmail.com>
2025-06-11 16:00:20 -0700

    Initial Commit

---
+++ myfinances/myfinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfinancesApp.swift
+//  myfinances
+//
+//  Created by Paolo Cremonese on 2025-06-11.
+//
+
+import SwiftUI
+
+@main
+struct myfinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myfinancesTests/myfinancesTests.swift
@@ -0,0 +1,17 @@
+//
+//  myfinancesTests.swift
+//  myfinancesTests
+//
+//  Created by Paolo Cremonese on 2025-06-11.
+//
+
+import Testing
+@testable import myfinances
+
+struct myfinancesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
